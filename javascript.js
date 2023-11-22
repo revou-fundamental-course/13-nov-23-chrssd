c1e6a62e19f43380d41d392985ee9aa0c1523b03
unknown <chriss>
2023-11-23 01:04:42 +0700

    Mini Project Revou

---
+++ javascript.js
@@ -0,0 +1,25 @@
+var slideIndex = 1;
+    showSlide(slideIndex);
+
+function nextslide(n){
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n){
+    var i;
+    var slides = document.getElementsByClassName("imgslide");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n<1){
+        slideIndex = slides.length;
+    }
+
+    for(i = 0; i <slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
